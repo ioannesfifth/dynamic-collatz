b2a819a34e6219d7ed8acb959bdff17ed3573b48
John Ryan Ramos <jrramos@freelancer.com>
2023-04-25 10:11:14 +0800

    add smarter sequencing

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 use std::{thread, time};
 
 fn collatz_condition(x: i32) -> i32 {
-    let one_millis = time::Duration::from_millis(1);
-    thread::sleep(one_millis);
+    // let one_millis = time::Duration::from_millis(1);
+    // thread::sleep(one_millis);
     if x % 2 == 0 {
         return x / 2
     } else {
@@ -39,12 +39,32 @@ fn find_collatz_sequence_dynamically(x: i32, collatz_sequences: Vec<Vec<i32>>) -
     return collatz_sequence;
 }
 
+fn find_collatz_sequence_dynamically_smarter(x: i32) -> Vec<i32> {
+    let mut collatz_sequence = Vec::new();
+    let mut p = x;
+
+    while p > 1 {
+        // println!("{}: {}", x, p);
+        if p < x {
+            break;
+        }
+        collatz_sequence.push(p);
+        p = collatz_condition(p);
+    }
+
+    return collatz_sequence;
+}
+
 pub fn run_collatz() {
-    let mut collatz_sequences = Vec::new();
+    // let mut collatz_sequences: Vec<Vec<i32>> = Vec::new();
     for x in 1..100 {
-        // let collatz_sequence = find_collatz_sequence(x);
-        let collatz_sequence = find_collatz_sequence_dynamically(x, collatz_sequences.clone());
-        collatz_sequences.push(collatz_sequence.clone());
+        // let collatz_sequence = 
+        // find_collatz_sequence(x);
+        // let collatz_sequence = 
+        // find_collatz_sequence_dynamically(x, collatz_sequences.clone());
+        // let collatz_sequence = 
+        find_collatz_sequence_dynamically_smarter(x);
+        // collatz_sequences.push(collatz_sequence.clone());
         // println!("{}: {:?}\n", x, collatz_sequence);
     }
 }
