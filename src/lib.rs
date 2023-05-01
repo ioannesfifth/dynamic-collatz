1929422916a23e966f4e6a7c6a6d40cc57a30255
Ioannes <johnramosver@gmail.com>
2023-05-01 22:14:41 +0800

    Basically just made it better

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
-use std::{thread, time};
+// use std::{thread, time};
+use std::collections::HashMap;
 
 fn collatz_condition(x: i32) -> i32 {
-    // let one_millis = time::Duration::from_millis(1);
-    // thread::sleep(one_millis);
+    // let one_nanos = time::Duration::from_nanos(1);
+    // thread::sleep(one_nanos);
     if x % 2 == 0 {
         return x / 2
     } else {
@@ -10,61 +11,90 @@ fn collatz_condition(x: i32) -> i32 {
     }
 }
 
-fn find_collatz_sequence(x: i32) -> Vec<i32> {
-    let mut collatz_sequence = Vec::new();
-    let mut p = x;
+// ------------------------------
+// Dynamic
+// ------------------------------
+// fn expand_sequence(p: i32, collatz_sequences: &HashMap<i32,Vec<i32>>) -> Vec<i32> {
+//     if p == 1 {
+//         return vec![2, 1];
+//     }
 
-    while p > 1 {
-        collatz_sequence.push(p);
-        p = collatz_condition(p);
+//     if collatz_sequences.contains_key(&p) {
+//         return collatz_sequences.get(&p).unwrap().to_vec();
+//     }
+
+//     let mut q = vec![p];
+//     q.extend(expand_sequence(collatz_condition(p), collatz_sequences));
+//     // let one_nanos = time::Duration::from_nanos(1);
+//     // thread::sleep(one_nanos);
+//     // println!("{}: {:?}", p, q);
+
+//     return q;
+// }
+
+
+// pub fn run_collatz() {
+//     let mut collatz_sequences: HashMap<i32,Vec<i32>> = HashMap::new();
+//     for x in 1..100 {
+//         let mut collatz_sequence = Vec::new();
+//         let mut p = x;
+    
+//         while p > 1 && !collatz_sequences.contains_key(&p) {
+//             let tmp_p = p;
+//             collatz_sequence.push(p);
+//             p = collatz_condition(p);
+//             collatz_sequences.insert(tmp_p, expand_sequence(p, &collatz_sequences));
+//         }
+//     }
+
+//     // println!("Collatz Sequences: {:?}", collatz_sequences);
+//     // for i in 1..(collatz_sequences.len() as i32 + 1) {
+//     //     let mut m: Vec<i32> = vec![];
+//     //     if collatz_sequences.contains_key(&i) {
+//     //         m = collatz_sequences.get(&i).unwrap().to_vec();
+//     //     }
+//     //     println!("{}: {:?}", i, m);
+//     // }
+// }
+
+// ------------------------------
+// Basic
+// ------------------------------
+fn expand_sequence(p: i32, collatz_sequences: &HashMap<i32,Vec<i32>>) -> Vec<i32> {
+    if p == 1 {
+        return vec![1];
     }
 
-    return collatz_sequence;
+    let mut q = vec![p];
+    q.extend(expand_sequence(collatz_condition(p), collatz_sequences));
+    // let one_nanos = time::Duration::from_nanos(1);
+    // thread::sleep(one_nanos);
+    // println!("{}: {:?}", p, q);
+
+    return q;
 }
 
-fn find_collatz_sequence_dynamically(x: i32, collatz_sequences: Vec<Vec<i32>>) -> Vec<i32> {
-    let mut collatz_sequence = Vec::new();
-    let mut p = x;
-
-    while p > 1 {
-        // println!("{}: {}", x, p);
-        if p < collatz_sequences.len().try_into().unwrap() {
-            collatz_sequence.extend(collatz_sequences[(p - 1) as usize].clone());
-            break;
-        }
-        collatz_sequence.push(p);
-        p = collatz_condition(p);
-    }
-
-    return collatz_sequence;
-}
-
-fn find_collatz_sequence_dynamically_smarter(x: i32) -> Vec<i32> {
-    let mut collatz_sequence = Vec::new();
-    let mut p = x;
-
-    while p > 1 {
-        // println!("{}: {}", x, p);
-        if p < x {
-            break;
-        }
-        collatz_sequence.push(p);
-        p = collatz_condition(p);
-    }
-
-    return collatz_sequence;
-}
 
 pub fn run_collatz() {
-    // let mut collatz_sequences: Vec<Vec<i32>> = Vec::new();
+    let mut collatz_sequences: HashMap<i32,Vec<i32>> = HashMap::new();
     for x in 1..100 {
-        // let collatz_sequence = 
-        // find_collatz_sequence(x);
-        // let collatz_sequence = 
-        // find_collatz_sequence_dynamically(x, collatz_sequences.clone());
-        // let collatz_sequence = 
-        find_collatz_sequence_dynamically_smarter(x);
-        // collatz_sequences.push(collatz_sequence.clone());
-        // println!("{}: {:?}\n", x, collatz_sequence);
+        let mut collatz_sequence = Vec::new();
+        let mut p = x;
+    
+        while p > 1 {
+            let tmp_p = p;
+            collatz_sequence.push(p);
+            p = collatz_condition(p);
+            collatz_sequences.insert(tmp_p, expand_sequence(p, &collatz_sequences));
+        }
     }
+
+    // println!("Collatz Sequences: {:?}", collatz_sequences);
+    // for i in 1..(collatz_sequences.len() as i32 + 1) {
+    //     let mut m: Vec<i32> = vec![];
+    //     if collatz_sequences.contains_key(&i) {
+    //         m = collatz_sequences.get(&i).unwrap().to_vec();
+    //     }
+    //     println!("{}: {:?}", i, m);
+    // }
 }
